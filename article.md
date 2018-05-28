8f075563d4bfdb93e8ac968f04174ce010a9e08f
Jan Boček <honza.bocek@gmail.com>
2018-05-28 13:08:53 +0200

    Verze k poznámkám

--- article.md
+++ article.md
@@ -1,41 +1,82 @@
-title: "Data z mobilů:<br>Praha se denně nafoukne o polovinu, v centru jsou návštěvníci v převaze"
-perex: "Skoro 140 tisíc lidí přijede do Prahy každý den za prací. Míří hlavně do kancelářských budov a do centra města. S nimi pak dorazí i přes půl milionu návštěvníků. Ukázala to analýza dat o pohybu mobilních telefonů, kterou si nyní město nechává zpracovat."
-published: "21. dubna 2018"
+title: "XXX"
+perex: "XXX"
+published: "29. května 2018"
 # autoři se zadávají až v redakčním systému
 coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
 coverimg_note: "Foto <a href='#'>ČTK</a>"
 styles: []
-libraries: [] #jquery, jquery-csv, d3, highcharts, datatables
-options: [noheader] #wide, noheader (, nopic)
+libraries: [jquery, highcharts] #jquery-csv, d3, , datatables
+options: [] #wide, noheader (, nopic)
 ---
-<left>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</left>
 
-Během dne se lidé v Praze soustřeďují v okolí administrativních center a obchodů, po poledni se tak nejvíc zahustí okolí Andělu, centrum Prahy (Vodičkova ulice a Petrské náměstí) a Brumlovka nedaleko Budějovické. Na poslední jmenované adrese sídlí řada velkých korporací, mezi nimi i Microsoft nebo ČEZ. Pro srovnání, na zmíněných místech je ve „špičce“ okolo šesti stovek osob na jeden hektar, průměr Prahy je 25 obyvatel na [hektar](https://cs.wikipedia.org/wiki/Hektar) (Václavské náměstí má rozlohu asi 4 hektary).
+Víc než polovina pokusů vystudovat bakalářský obor v Česku končí neúspěchem. Na technických školách je číslo ještě vyšší: například Technická fakulta České zemědělské univerzity má devadesátiprocentní studijní neúspěšnost.
 
-Naopak v noci se nejvíc lidí se „namačká“ ve Vršovicích, jde o 430 osob na hektar.
+„XXX,“ říká technická škola.
 
-Detaily si můžete prohlédnout v následující mapě, kterou z dat mobilních operátorů zpracoval pražský [Institut plánování a rozvoje](http://www.iprpraha.cz/).
+Neúspěšnost navíc od zavedení bakalářského stupně roste. V imatrikulačním ročníku 2001 – jednom z prvních, který měl možnost hlásit se na bakaláře – skončilo neúspěšně lehce přes 40 procent studií. O deset let později to bylo 55 procent a číslo se dál zvyšuje.
 
-<wide>_Pokud vás zajímá, kolik lidí bývá ve dne a v noci ve vaší čtvrti, najděte si ji na mapě, přepněte se na časové řezy, vyberte přepínač hustota osob a potom pohybujte posuvníkem nahoře._</wide>
+<wide><div id="casovarada" style="max-width: 100%; height: 500px"></div></wide>
 
+„Veřejné investice do systému vysokého školství jsou vyšší mírou studijní neúspěšnosti znehodnoceny,“ vysvětluje publikace [Studijní neúspěšnost na vysokých školách]( https://www.databazeknih.cz/knihy/studijni-neuspesnost-na-vysokych-skolach-teoreticka-vychodiska-empiricke-poznatky-a-doporuceni-355059) jeden z nejzávažnějších důvodů, proč se tématu věnovat. „Zaměstnavatelé zase dlouhodobě kritizují nedostatek kvalitních absolventů ve vybraných oborech – často právě v těch, které vykazují vysokou míru studijní neúspěšnosti.“
 
-## Data na prodej
-<right>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</right>
+„Řada lidí, hlavně přímo z vysokých škol, považuje studijní úmrtnost za přirozený jev,“ dodává Šimon Stibůrek, jeden ze spoluautorů a ex-analytik na ministerstvu školství. „Často ji dokonce prezentují jako indikátor kvality a zachování vysoké úrovně školy. Ve skutečnosti vypovídá spíš o tom, že vysoká škola svým studentům nerozumí a nechce se jim přizpůsobovat. Školy příliš nevnímají, že studenti jsou jiní než třeba před dvaceti lety a když obor nenaplní jejich představy, prostě odejdou.“
 
-I pokud netelefonujeme nebo neposíláme SMS, telefon pravidelně komunikuje s vysílači v síti mobilního operátora. Z dat o síle signálu a použité anténě je možné odvodit, kde se přibližně telefon nachází a jak dlouhou dobu na místě strávil.
+## Technické fakulty mají neúspěšnost i 90 procent
 
-Mobilní operátor má [zákonnou povinnost](https://www.zakonyprolidi.cz/cs/2005-127/zneni-20160919#p97-3) takové informace uchovávat, a to půl roku do minulosti, přistupovat k nim ale mohou jen bezpečnostní složky, typicky policie nebo rozvědka.
+Studentů, kteří bakaláře nedokončí, je méně, „jen“ kolem čtyřiceti procent. Ukončené studium totiž obvykle neznamená definitivní neúspěch, častěji jde o přechod na jiný obor nebo jinou školu.
 
-Operátoři ale obdobné informace mohou anonymizovat (tedy zbavit identifikace konkrétního uživatele) a agregovat, tedy spojit informace o jednotlivcích do jakýchsi skupin. Výsledná obecná čísla pak přeprodávají dál, například marketingovým firmám či dopravním analytikům. Zpětně z nich nejde dovodit, kde se pohybuje každý jednotlivec, dávají ale určitý přehled o obecném chování obyvatel nějakého místa.
+„Velká část odchodů je takzvanou odloženou volbou, kdy studenti nastoupili do více studií a jedno z nich opustí,“ tvrdí zmíněná publikace. „Ta je motivována především nesouladem představ a očekávání od studia a jeho skutečné podoby.“
+
+„A naopak, školy často využívají první ročník jako prodloužené přijímací řízení,“ doplňuje Stibůrek. „Přijímací zkoušky použijí jen jako hrubé síto a studenty často protřídí třeba na konci prváku. Tím opět přispívají k nízké efektivitě systému. Student zjistí až po roce, že ho vlastně škola nechtěla.“
+
+Hypotézy o odložené volbě i prodlouženém přijímacím řízení potvrzují také data o tom, kdy studenti školu ukončí. Naprostá většina z nich odejde v prvním ročníku. Je to patrné zvlášť na technických oborech: například na Fakultě jaderné a fyzikálně inženýrské Českého vysokého učení technického s téměř devadesátiprocentní studijní neúspěšností skončí čtyři z pěti studentů hned v prvním ročníku.
+
+<wide><div id="technicke" style="max-width: 100%; height: 600px"></div></wide>
+
+„Koncentrace neúspěšnosti do prvního ročníku je často v přímém rozporu s intuicí mnoha akademických hodnostářů i odborníků zabývajících se vysokým školstvím,“ upozorňují autoři publikace o studijní úmrtnosti. „Řada z nich se naopak domnívá, že neúspěšnost se soustředí zejména v posledních ročnících studia, v bodě, kdy studenti musejí splnit dříve odložené studijní povinnosti, napsat závěrečnou práci a složit státní závěrečné zkoušky. Data ale žádný takový vývoj nepotvrzují. Studijní neúspěšnost ve třetích a čtvrtých ročnících bakalářského studia je ve srovnání s prvním a druhým ročníkem zanedbatelná.“
+
+## Mezi vyspělými zeměmi patříme k těm s nejvyšší neúspěšností
+
+Vysokou neúspěšností v Česku trpí hlavně bakalářské obory. Na pětiletých a obdobných magisterských studiích je nižší. Za rozdíl může zejména oborová struktura „dlouhého“ magistra – jde obvykle o výběrovější studia medicíny, práva nebo psychologie. Do bakalářských studií se zároveň koncentruje přibližně desetkrát více studentů než do dlouhých magisterských problémů, proto je právě zde problém se studijní úspěšností bakalářů podstatnější.
+
+Navazující, „krátké“ magisterské studium, které následuje po úspěšném absolvování bakaláře, má už úspěšnost poměrně vysokou.
+
+Naopak nejnižší úspěšnost mají doktorské programy – příčinou jsou nejčastěji nízké příjmy a tlak na učení, kvůli kterému se doktorandi méně soustředí na vědeckou práci.
+
+<div id="programy" style="max-width: 100%; height: 400px"></div>
+
+Čeští bakaláři jsou zároveň neúspěšní častěji, než je obvyklé ve vyspělých zemích. Srovnání s výběrem zemí OECD, pro které máme kompletní data, ukazuje, že po Estonsku a Švédsku jsme třetí nejhorší. Prozrazují to data o imatrikulačním ročníku 2013, který by letos překročil standardní dobu studia o dva roky a můžeme tak předpokládat, že čísla už se nebudou příliš měnit.
+
+<div id="mezinarodni" style="max-width: 100%; height: 400px"></div>
+
+## Technické obory mají úmrtnost přes 60 procent, psychologie pod 15 procent
+
+Data o studijní úmrtnosti sice umožňují srovnání univerzit, to by ale nedávalo smysl: největší rozdíly jsou mezi skupinami oborů a u univerzit tak záleží spíš na oborové struktuře, než na jiných faktorech. U technických, přírodovědných a zemědělských bakalářských oborů se neúspěšnost pohybuje kolem 60 procent, u humanitních a ekonomických oborů mezi 40 a 50 procenty, ve zdravotnických, právních, pedagogických a uměleckých oborech kolem 30 procent, u psychologů pod 15 procenty.
+
+Zajímavější je proto ukazovat neúspěšnost fakult v rámci jedné vysoké školy. Například u Univerzity Karlovy mají nejnižší neúspěšnost lékařské fakulty, nejvyšší předvídatelně Matematicko-fyzikální fakulta a poněkud překvapivě také Fakulta humanitních studií.
+
+<div id="uk" style="max-width: 100%; height: 400px"></div>
+
+„XXX,“ říká někdo k FHS UK. (Fakulta hledající smysl)
+
+Ještě zajímavější je srovnání fakult stejného zaměření napříč univerzitami. Vedle porovnání technických oborů, které najdete výše, má zajímavé výsledky také srovnání pedagogických fakult.
+
+<div id="pedagogicke" style="max-width: 100%; height: 400px"></div>
+
+Neúspěšnost přes 60 procent u liberecké univerzity je mezi netechnickými obory neobvykle vysoká. V porovnání se soukromou UJAK víc než trojnásobná.
+
+„XXX,“ říká TUL.
+
+## Soustřeďte se na první rok, vzkazují školám analytici
+
+„Tradičně se mělo za to, že uchazeč o vysokou školu – a potažmo student vysoké školy – je dospělý člověk a je jen na něm, jak ke svému studiu přistoupí,“ shrnuje publikace Studijní neúspěšnost na vysokých školách svoje zjištění. „To do velké míry platí dodnes. Ke zrání osobnosti přispívá, pokud člověk trochu experimentuje a zároveň si nese důsledky tohoto experimentování. Nevhodný výběr školy, tedy ‚nepodařený experiment‘, nemusí být nutně ztracený čas. Může jít o příležitost poznat lépe sám sebe.“
+
+Zároveň ale studie dodává, že takové experimentování je drahé, mělo by zčásti probíhat ještě na střední škole a má efekt pouze tehdy, když školy aktivně s motivací studentů pracují. To podle nich školy dosud příliš neumí.
+
+„Právě první ročníky by měly být v centru pozornosti výzkumníků, vysokoškolských institucí i tvůrců politik,“ vzkazují autoři studie. „Měli bychom se snažit zjistit, jaká je zkušenost studentů právě v prvním ročníku, jací studenti z něj odcházejí, z jakých důvodů a nakolik může být tento vývoj ovlivněn národní politikou nebo opatřeními na úrovni jednotlivých vysokých škol, fakult, kateder a předmětů.“
+
+Podle analytika Šimona Stibůrka to znamená věnovat se nejen samotnému vzdělávání, ale také sociálnímu prostředí na vysokých školách. Podpora spolužáků je důležitá například pro ty, jejichž rodiče sami vysokou školu nemají a hůř tak hledají motivaci v rodině. Studijní neúspěšnost obvykle postihuje sociálně znevýhodněné skupiny a brání sociální mobilitě.
+
+„Je čím dál větší propast mezi tím, co studenti chtějí, a co jim školy nabízejí,“ vysvětluje Stibůrek. „Školy už se naučily občas se zeptat studentů na jejich očekávání, ale neumí s ním zatím moc pracovat. Dřív školy odchody studentů tolik netrápily, teď je ohrožují: na školy nastupují slabší ročníky a tak chybějí i peníze.“
+
+„Školy teď řeší střet mezi dvěma přístupy,“ pokračuje Stibůrek. „Klasický elitní, kdy dokončit mají jenom ti nejlepší, a pragmatický, kdy škola nechce studenty vyhazovat kvůli penězům. V klasickém pojetí je pro školu podstatná kvalita absolventů. Moderní pojetí říká, že je důležité, aby absolventi uměli víc, než když přišli. Ptá se, jestli jsou peníze vložené do vzdělávání efektivně využité.“
--- js/script.js
+++ js/script.js
@@ -1,1 +1,476 @@
-// JS soubory ve složce /styles/ se do výsledného článku zakompilují automaticky
+var colors = ['#EA614A', '#20649B', '#008836', '#6B96CA', '#A38456', '#A87A93', '#D19C95']
+
+var neuspesnostZeme = [
+  ["Estonsko",43.4],
+  ["Švédsko",33.6],
+  ["Česko", 30.6],
+  ["Izrael",25.9],
+  ["Rakousko",23.6],
+  ["Belgie (Vlámsko)",22.1],
+  ["Nizozemsko",22],
+  ["Francie",21.2],
+  ["Norsko",21.2],
+  ["Finsko",20.8],
+  ["Austrálie",19.6],
+  ["USA",16.6],
+  ["Nový Zéland",16.1],
+  ["Velká Británie",15.9],
+  ["Dánsko",13.1]
+]
+
+var zeme = neuspesnostZeme.map(function(d) {
+        return d[0];
+    });
+
+var rok0 = [[5.5],[5.2],[5.6],[5.0],[5.9],[5.4],[5.2],[6.3],[5.9],[5.8],[5.8],[5.2],[5.6],[5.4],[5.7],[5.7],[5.7]],
+    rok1 = [[22.2],[21.8],[24.5],[24.6],[26.0],[28.8],[27.2],[28.2],[29.3],[30.3],[31.1],[32.0],[32.3],[35.0],[33.7],[34.0]],
+    rok2 = [[7.3],[8.7],[8.8],[10.6],[10.2],[9.5],[9.1],[8.9],[9.2],[9.9],[9.7],[9.8],[10.4],[10.1],[11.0]],
+    rok3 = [[3.7],[4.3],[4.9],[5.0],[4.5],[4.4],[4.4],[4.6],[4.5],[4.3],[4.5],[4.8],[4.8],[4.7]],
+    rok4 = [[2.0],[2.4],[2.3],[2.4],[2.3],[2.4],[2.5],[2.6],[2.4],[2.6],[2.8],[2.7],[3.0]],
+    rok5 = [[1.4],[1.3],[1.3],[1.1],[1.3],[1.3],[1.3],[1.2],[1.4],[1.3],[1.5],[1.7]],
+    rok6 = [[1.4],[1.4],[1.1],[1],[0.9],[0.9],[1.1],[1.1],[1],[1],[0.9]]
+
+var roky = [2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+
+var rok0UK = [[3.1],[3.0],[15.5],[2.8],[2.2],[3.3],[2.1],[4.0],[5.5],[2.8],[2.9],[3.6],[2.9],[0.0],[9.1],[1.8]],
+    rok1UK = [[52.7],[52.1],[24.0],[37.6],[32.0],[31.2],[32.9],[25.8],[29.3],[19.2],[16.6],[21.6],[24.6],[26.7],[15.2],[12.6]],
+    rok2UK = [[16.2],[4.8],[13.2],[6.9],[8.7],[8.8],[7.2],[9.3],[6.1],[10.9],[10.8],[11.3],[5.2],[3.3],[7.6],[7.2]],
+    rok3UK = [[4.8],[3.2],[3.1],[3.8],[5.9],[4.4],[3.2],[5.1],[1.7],[7.2],[2.9],[3.6],[3.3],[0.0],[1.5],[5.4]],
+    rok4UK = [[3.1],[1.8],[4.7],[6.2],[5.5],[3.7],[2.6],[2.5],[3.9],[3.7],[8.6],[1.4],[5.0],[6.7],[1.5],[6.6]]
+
+var fakultyUK = ["FHS","MFF","FaF","HTF","FTVS","celá UK","PřF","PedF","ETF","FSV","KTF","1.LF","FF","LF HK","2.LF","3.LF"]
+
+var rok0TF = [[19.0],[8.9],[4.5],[7.4],[9.5],[10.8],[5.8],[2.5],[2.4],[2.7],[8.2],[10.0],[3.2],[3.3],[2.0],[5.4],[8.1],[4.8],[6.2],[5.5],[2.7],[4.8],[4.5],[7.4],[4.4],[2.9],[6.3],[3.3],[5.6],[2.4],[4.8],[8.6],[3.8],[19.1],[5.6],[9.8],[2.1],[10.4],[3.2],[5.4],[3.0],[2.0],[0.0],[2.0]],
+    rok1TF = [[48.1],[60.1],[73.3],[67.8],[58.3],[49.9],[55.6],[67.0],[47.7],[44.2],[51.0],[50.3],[65.0],[52.1],[59.1],[57.3],[54.6],[51.1],[49.9],[51.2],[47.5],[50.0],[49.5],[47.8],[42.7],[56.5],[35.0],[32.2],[35.8],[42.7],[36.9],[19.9],[33.3],[16.6],[33.1],[33.2],[34.0],[33.2],[36.2],[32.1],[18.8],[13.3],[16.3],[8.9]],
+    rok2TF = [[17.7],[7.5],[6.2],[7.9],[12.6],[12.3],[14.5],[8.5],[22.9],[20.1],[7.8],[8.5],[6.5],[16.6],[7.9],[4.6],[7.4],[12.0],[11.1],[10.2],[16.0],[10.2],[8.8],[10.2],[6.0],[4.3],[11.5],[19.2],[10.5],[11.0],[10.2],[11.6],[16.7],[13.1],[11.4],[9.5],[14.2],[10.1],[9.8],[6.7],[13.8],[10.1],[4.1],[2.0]],
+    rok3TF = [[4.4],[7.8],[2.2],[2.4],[4.6],[8.7],[2.7],[1.5],[2.6],[4.6],[6.3],[4.5],[1.1],[2.3],[3.9],[4.4],[3.7],[3.8],[4.4],[2.4],[2.3],[2.3],[3.6],[1.6],[6.5],[1.9],[6.8],[7.2],[4.6],[4.0],[5.3],[9.8],[4.3],[4.9],[4.5],[3.6],[3.8],[0.9],[2.8],[6.4],[6.5],[3.5],[4.1],[2.0]],
+    rok4TF = [[1.1],[3.4],[1.2],[1.3],[0.6],[0.5],[2.2],[1.0],[1.1],[4.6],[2.9],[2.6],[0.0],[1.3],[2.5],[3.5],[1.4],[2.3],[1.0],[1.4],[1.3],[1.6],[1.7],[0.8],[7.0],[0.5],[4.8],[1.4],[5.9],[2.2],[2.7],[9.9],[1.5],[4.4],[3.4],[1.2],[1.1],[0.2],[2.8],[2.6],[4.0],[2.0],[6.1],[2.0]];
+
+var fakultyTF = ["TF ČZU","FVTM UJEP","FJFI ČVUT","FS TUL","FT UTB","FS ZČU","DFJP UPa","FTOP VŠCHT","FD ČVUT","FEI UPa","FAV ZČU","FAI UTB","FCH VUT","FIT ČVUT","FMIM TUL","FT TUL","FMMI VŠB-TUO","FE ZČU","FBI VŠB-TUO","FCHT VŠCHT","FAST VŠB-TUO","FPBT ČVUT","HGF VŠB-TUO","VŠPJ","FEI VŠB-TUO","FCHI ČVUT","FI MU","FS ČVUT","FCHT UPa","FIM UHK","FEL ČVUT","VŠTE","FS VŠB-TUO","FBMI ČVUT","FEKT VUT","FIS VŠE","FSv ČVUT","FLKŘ UTB","FS VUT","FSI VUT","FIT VUT","FA ČVUT","FUA TUL","FA VUT"]
+
+var rok0PF = [[4.9],[4.9],[6.4],[5.5],[4.0],[3.8],[8.4],[4.5],[2.1],[1.2]],
+    rok1PF = [[31.9],[32.5],[24.6],[24.4],[25.8],[23.3],[17.9],[15.6],[17.6],[11.5]],
+    rok2PF = [[9.7],[9.1],[12.0],[6.9],[9.3],[7.9],[7.8],[11.4],[7.3],[3.6]],
+    rok3PF = [[8.1],[9.2],[5.1],[7.1],[5.1],[5.3],[5.8],[5.0],[4.8],[1.4]],
+    rok4PF = [[6.1],[2.2],[4.9],[4.7],[2.5],[3.1],[1.6],[3.9],[4.1],[1.8]];
+
+var fakultyPF = ["TUL","ZČU","MU","UJEP","UK","JČU","OUO","UPOL","UHK","UJAK"];
+
+var programy = [[61.4], [67.9], [84.3], [34.1]]
+
+
+
+$(function () {
+
+Highcharts.chart('mezinarodni', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní neúspěšnost bakalářů, Česko × OECD'
+    },
+    subtitle: {
+        text: 'srovnání studentů, kteří nastoupili 2013, letos by standardní dobu studia překročili o 2 roky'
+    },
+    xAxis: {
+        categories: zeme
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní neúspěšnost Bc. studia'
+        },
+        labels: {
+            format: '{value} %'
+        }
+    },
+    tooltip: {
+        pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} %</b><br/>',
+        crosshairs: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    legend: {
+        enabled: false
+    },
+    series: [{
+        name: 'bakaláře nedokončilo',
+        data: [
+            neuspesnostZeme[0],
+            neuspesnostZeme[1],
+            {y: neuspesnostZeme[2][1], color: colors[1]},
+            neuspesnostZeme[3],
+            neuspesnostZeme[4],
+            neuspesnostZeme[5],
+            neuspesnostZeme[6],
+            neuspesnostZeme[7],
+            neuspesnostZeme[8],
+            neuspesnostZeme[9],
+            neuspesnostZeme[10],
+            neuspesnostZeme[11],
+            neuspesnostZeme[12],
+            neuspesnostZeme[13],
+            neuspesnostZeme[14]
+        ],
+        color: colors[0]
+    }]
+});
+
+Highcharts.chart('casovarada', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní neúspěšnost bakalářů, časová řada'
+    },
+    subtitle: {
+        text: 'srovnání neúspěšně ukončených studií podle ročníku nástupu'
+    },
+    xAxis: {
+        categories: roky
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní neúspěšnost Bc. studia'
+        },
+        labels: {
+            format: '{value} %'
+        },
+        reversedStacks: false
+    },
+    tooltip: {
+        formatter: function () {
+            var s = 'Začátek studia: <b>' + this.x + '</b>',
+                celkem = 0;
+
+            $.each(this.points, function () {
+                s += '<br/><span style="color:' + this.color +'">' + this.series.name + '</span> neuspělo <strong>' +
+                    this.y + ' %</strong>';
+                celkem += this.y;
+            });
+
+            s += '<br/>Dosud neúspěšně ukončilo studium <b>' + Math.round(celkem*10)/10 + '%</b>';
+
+            return s;
+        },
+        shared: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    series: [{
+        name: '0. rok',
+        data: rok0,
+        color: colors[0]
+    }, {
+        name: '1. rok',
+        data: rok1,
+        color: colors[1]
+    }, {
+        name: '2. rok',
+        data: rok2,
+        color: colors[2]
+    }, {
+        name: '3. rok',
+        data: rok3,
+        color: colors[3]
+    }, {
+        name: '4. rok',
+        data: rok4,
+        color: colors[4]
+    }, {
+        name: '5. rok',
+        data: rok5,
+        color: colors[5]
+    }, {
+        name: '6. rok a později',
+        data: rok6,
+        color: colors[6]
+    }]
+});
+
+Highcharts.chart('uk', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní neúspěšnost bakalářů, Univerzita Karlova'
+    },
+    subtitle: {
+        text: 'srovnání neúspěšně ukončených studií podle fakulty, rok nástupu 2013'
+    },
+    xAxis: {
+        categories: fakultyUK
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní neúspěšnost Bc. studia'
+        },
+        labels: {
+            format: '{value} %'
+        },
+        reversedStacks: false
+    },
+    tooltip: {
+        formatter: function () {
+            var s = 'Fakulta: <b>' + this.x + '</b>',
+                celkem = 0;
+
+            $.each(this.points, function () {
+                s += '<br/><span style="color:' + this.color +'">' + this.series.name + '</span> neuspělo <strong>' +
+                    this.y + ' %</strong>';
+                celkem += this.y;
+            });
+
+            s += '<br/>Dosud neúspěšně ukončilo studium <b>' + Math.round(celkem*10)/10 + '%</b>';
+
+            return s;
+        },
+        shared: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    series: [{
+        name: '0. rok',
+        data: rok0UK,
+        color: colors[0]
+    }, {
+        name: '1. rok',
+        data: rok1UK,
+        color: colors[1]
+    }, {
+        name: '2. rok',
+        data: rok2UK,
+        color: colors[2]
+    }, {
+        name: '3. rok',
+        data: rok3UK,
+        color: colors[3]
+    }, {
+        name: '4. rok',
+        data: rok4UK,
+        color: colors[4]
+    }]
+});
+
+Highcharts.chart('technicke', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní neúspěšnost bakalářů, technické fakulty'
+    },
+    subtitle: {
+        text: 'srovnání neúspěšně ukončených studií podle fakulty, rok nástupu 2013'
+    },
+    xAxis: {
+        categories: fakultyTF
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní neúspěšnost Bc. studia'
+        },
+        labels: {
+            format: '{value} %'
+        },
+        reversedStacks: false
+    },
+    tooltip: {
+        formatter: function () {
+            var s = 'Fakulta: <b>' + this.x + '</b>',
+                celkem = 0;
+
+            $.each(this.points, function () {
+                s += '<br/><span style="color:' + this.color +'">' + this.series.name + '</span> neuspělo <strong>' +
+                    this.y + ' %</strong>';
+                celkem += this.y;
+            });
+
+            s += '<br/>Dosud neúspěšně ukončilo studium <b>' + Math.round(celkem*10)/10 + '%</b>';
+
+            return s;
+        },
+        shared: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    series: [{
+        name: '0. rok',
+        data: rok0TF,
+        color: colors[0]
+    }, {
+        name: '1. rok',
+        data: rok1TF,
+        color: colors[1]
+    }, {
+        name: '2. rok',
+        data: rok2TF,
+        color: colors[2]
+    }, {
+        name: '3. rok',
+        data: rok3TF,
+        color: colors[3]
+    }, {
+        name: '4. rok',
+        data: rok4TF,
+        color: colors[4]
+    }]
+});
+
+Highcharts.chart('pedagogicke', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní neúspěšnost bakalářů, pedagogické fakulty'
+    },
+    subtitle: {
+        text: 'srovnání neúspěšně ukončených studií podle fakulty, rok nástupu 2013'
+    },
+    xAxis: {
+        categories: fakultyPF
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní neúspěšnost Bc. studia'
+        },
+        labels: {
+            format: '{value} %'
+        },
+        reversedStacks: false
+    },
+    tooltip: {
+        formatter: function () {
+            var s = 'Fakulta: <b>' + this.x + '</b>',
+                celkem = 0;
+
+            $.each(this.points, function () {
+                s += '<br/><span style="color:' + this.color +'">' + this.series.name + '</span> neuspělo <strong>' +
+                    this.y + ' %</strong>';
+                celkem += this.y;
+            });
+
+            s += '<br/>Dosud neúspěšně ukončilo studium <b>' + Math.round(celkem*10)/10 + '%</b>';
+
+            return s;
+        },
+        shared: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        }
+    },
+    series: [{
+        name: '0. rok',
+        data: rok0PF,
+        color: colors[0]
+    }, {
+        name: '1. rok',
+        data: rok1PF,
+        color: colors[1]
+    }, {
+        name: '2. rok',
+        data: rok2PF,
+        color: colors[2]
+    }, {
+        name: '3. rok',
+        data: rok3PF,
+        color: colors[3]
+    }, {
+        name: '4. rok',
+        data: rok4PF,
+        color: colors[4]
+    }]
+});
+
+Highcharts.chart('programy', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Studijní úspěšnost Bc. × Mgr. × PhD.'
+    },
+    subtitle: {
+        text: 'srovnání úspěšně ukončených studií, rok nástupu 2010'
+    },
+    xAxis: {
+        categories: ['Bc.', 'Mgr.', 'navazující Mgr.', 'PhD.']
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'studijní úspěšnost'
+        },
+        labels: {
+            format: '{value} %'
+        }
+    },
+    tooltip: {
+        pointFormat: '<span style="color:{point.color}">\u25CF</span> {series.name}: <b>{point.y} %</b><br/>',
+        crosshairs: true
+    },
+    exporting: {
+        enabled: false
+    },
+    credits: {
+        href: '',
+        text: 'Zdroj: MŠMT'
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {},
+    series: [{
+        name: 'Studijní úspěšnost',
+        data: programy,
+        color: colors[2]
+    }]
+});
+
+});
